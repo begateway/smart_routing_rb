6c0899e123c2ce92bc4048014ac4c8c6e4e5bdce
Mikhail Davidovich <mihaildv@gmail.com>
2018-10-15 10:33:44 +0300

    add configuration for api_host, logger, open_tomeout and read_tomeout

--- lib/smart_routing.rb
+++ lib/smart_routing.rb
@@ -1,5 +1,15 @@
 require "smart_routing/version"
 
 module SmartRouting
-  # Your code goes here...
+
+  @api_host = "https://api.smart-routing.ecomcharge.com"
+
+  @logger = nil
+
+  @open_tomeout = 20
+  @read_tomeout = 40
+
+  class << self
+    attr_accessor :api_host, :logger, :open_tomeout, :read_tomeout
+  end
 end
--- spec/smart_routing_spec.rb
+++ spec/smart_routing_spec.rb
@@ -2,4 +2,58 @@ RSpec.describe SmartRouting do
   it "has a version number" do
     expect(SmartRouting::VERSION).not_to be nil
   end
+
+  context "configuration" do
+    let(:logger) { double(:logger) }
+
+    it "allows api_host to be configured" do
+      begin
+        old = SmartRouting.api_host
+        SmartRouting.api_host = "https://api.host.com"
+
+        expect(SmartRouting.api_host).to eq("https://api.host.com")
+      ensure
+        SmartRouting.api_host = old
+      end
+    end
+
+    it "allows logger to be configured" do
+      begin
+        old = SmartRouting.logger
+        SmartRouting.logger = logger
+
+        expect(SmartRouting.logger).to eq(logger)
+      ensure
+        SmartRouting.logger = old
+      end
+    end
+
+    it "allows open_tomeout to be configured" do
+      begin
+        old = SmartRouting.open_tomeout
+        SmartRouting.open_tomeout = 5
+
+        expect(SmartRouting.open_tomeout).to eq(5)
+      ensure
+        SmartRouting.open_tomeout= old
+      end
+    end
+
+    it "allows read_tomeout to be configured" do
+      begin
+        old = SmartRouting.read_tomeout
+        SmartRouting.read_tomeout = 10
+
+        expect(SmartRouting.read_tomeout).to eq(10)
+      ensure
+        SmartRouting.read_tomeout= old
+      end
+    end
+
+    it "has default open_tomeout and read_tomeout" do
+      expect(SmartRouting.open_tomeout).to eq(20)
+      expect(SmartRouting.read_tomeout).to eq(40)
+    end
+
+  end
 end
